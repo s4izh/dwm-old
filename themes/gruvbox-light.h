f01a104635d6f2c64bc49db391b5963925857dd5
Sergio Sanz <sergiosanz234@gmail.com>
2023-01-29 13:47:01 +0100

    added gruvbox light

--- themes/gruvbox-light.h
+++ themes/gruvbox-light.h
@@ -1,9 +1,9 @@
 static char normbgcolor[]           = "#fbf1c7";
 static char normbordercolor[]       = "#282828";
 static char normfgcolor[]           = "#282828";
-static char selfgcolor[]            = "#c14a4a";
+static char selfgcolor[]            = "#282828";
 static char selbordercolor[]        = "#282828";
-static char selbgcolor[]            = "#fbf1c7";
+static char selbgcolor[]            = "#ebdbb2";
 
 static char *colors[][3] = {
                      /* fg           bg           border */
