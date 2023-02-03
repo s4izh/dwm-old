cd6d6a6a56166801e1a34de2ddb4b01269cd92f4
Sergio Sanz <sergiosanz234@gmail.com>
2023-02-03 18:24:43 +0100

    new themes

--- themes/red.h
+++ themes/red.h
@@ -1,9 +1,9 @@
-static char normbgcolor[]           = "#222222";
+static char normbgcolor[]           = "#101010";
 static char normbordercolor[]       = "#444444";
 static char normfgcolor[]           = "#bbbbbb";
 static char selfgcolor[]            = "#eeeeee";
-static char selbordercolor[]        = "#ffffff";
-static char selbgcolor[]            = "#005577";
+static char selbordercolor[]        = "#780000";
+static char selbgcolor[]            = "#780000";
 
 static char *colors[][3] = {
                      /* fg           bg           border */
