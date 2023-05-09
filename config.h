af0ff1d3915574c6c3bbc0d876a7c4e0a37a6b6d
s4izh <sergiosanz234@gmail.com>
2023-05-09 20:44:02 +0200

    default font mono

--- config.h
+++ config.h
@@ -13,7 +13,8 @@ static const unsigned int systrayspacing = 2;   /* systray spacing */
 static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
 static const int showsystray        = 1;     /* 0 means no systray */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "JetBrainsMono NerdFont:size=10", "JoyPixels:pixelsize=12:antialias=true:autohint=true" };
+/* static const char *fonts[]          = { "JetBrainsMono NerdFont:size=10", "JoyPixels:pixelsize=12:antialias=true:autohint=true" }; */
+static const char *fonts[]          = { "mono:size=10", "JoyPixels:pixelsize=12:antialias=true:autohint=true" };
 static const char dmenufont[]       = "JetBrainsMono NerdFont:size=10";
 
 #include "theme.h"
