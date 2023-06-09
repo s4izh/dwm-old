d45ff52f692665c19266013bb631970d8c9e6f21
s4izh <sergiosanz234@gmail.com>
2023-06-09 19:28:57 +0200

    sourcerer theme

--- themes/sourcerer.h
+++ themes/sourcerer.h
@@ -1,8 +1,8 @@
 static char normbgcolor[]           = "#171717";
 static char normbordercolor[]       = "#444444";
-static char normfgcolor[]           = "#f1f1f1";
+static char normfgcolor[]           = "#bbbbbb";
 static char selfgcolor[]            = "#7D8FA3";
-static char selbordercolor[]        = "#f1f1f1";
+static char selbordercolor[]        = "#bbbbbb";
 static char selbgcolor[]            = "#171717";
 static char *colors[][3] = {
        [SchemeNorm] = { normfgcolor, normbgcolor, normbgcolor },
